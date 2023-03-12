596cc75c31815c00b1401bc804135e59d19f2a9e
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-03-12 22:00:09 +0900

    Setup _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,16 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Handred-Square Calculations</title>
+        <meta name="description" content="handred-square calculations" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,6 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Handred-Square Calculations</title>
-        <meta name="description" content="handred-square calculations" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main>Hello World!</main>
     </>
   );

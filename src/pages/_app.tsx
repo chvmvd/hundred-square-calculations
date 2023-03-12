9a73586091d126b29731743952f76a625a5a0b56
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-03-12 22:11:03 +0900

    Fix typos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Handred-Square Calculations</title>
+        <title>Hundred-Square Calculations</title>
         <meta name="description" content="handred-square calculations" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />

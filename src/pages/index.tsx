8fa15665578a196695af0bf1367e45fb48938ef9
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-03-16 22:35:00 +0900

    Create sum

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,48 @@
+import { useState, useEffect } from "react";
+import { InlineMath } from "react-katex";
+import "katex/dist/katex.min.css";
+
+function calculateRandomNumbers() {
+  return [...Array(9)].map((_) => Math.ceil(Math.random() * 9));
+}
+
 export default function Home() {
+  const [leftNumbers, setLeftNumbers] = useState([...Array(9)].map(() => 1));
+  const [rightNumbers, setRightNumbers] = useState([...Array(9)].map(() => 1));
+  useEffect(() => {
+    setLeftNumbers(calculateRandomNumbers());
+    setRightNumbers(calculateRandomNumbers());
+  }, []);
   return (
     <>
-      <main>Hello World!</main>
+      <main>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <InlineMath>+</InlineMath>
+              </td>
+              {leftNumbers.map((leftNumber, i) => (
+                <td key={i}>
+                  <InlineMath>{`${leftNumber}`}</InlineMath>
+                </td>
+              ))}
+            </tr>
+            {rightNumbers.map((rightNumber, i) => (
+              <tr key={i}>
+                <td>
+                  <InlineMath>{`${rightNumber}`}</InlineMath>
+                </td>
+                {leftNumbers.map((leftNumber, j) => (
+                  <td key={`${i},${j}`}>
+                    <InlineMath>{`${leftNumber + rightNumber}`}</InlineMath>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
     </>
   );
 }

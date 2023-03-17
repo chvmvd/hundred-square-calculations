edc7b800d0bc54141c3900ed51714497b5355788
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-03-17 21:22:40 +0900

    Create AppBar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,15 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
+import NextLink from "next/link";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Stack,
+  Link,
+  IconButton,
+} from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +20,31 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h5" component="div" flexGrow={1}>
+            Hundred-Square Calculations
+          </Typography>
+          <Stack direction="row" spacing={2} alignItems="center">
+            <Link
+              href="/"
+              component={NextLink}
+              color="inherit"
+              underline="hover"
+              sx={{ fontSize: "large" }}
+            >
+              Home
+            </Link>
+            <IconButton
+              color="inherit"
+              href="https://github.com/chvmvd/hundred-square-calculations"
+            >
+              <GitHubIcon fontSize="large" />
+            </IconButton>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
       <Component {...pageProps} />
     </>
   );
